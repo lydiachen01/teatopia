8294b6a33c9d9699ac74f9b2dc2dcfb8767f09e6
lydiachen01 <137934459+lydiachen01@users.noreply.github.com>
2023-12-08 17:36:59 -0500

    Implemented user_auth.php and create_user.php + other updates

---
+++ create_user.php
@@ -0,0 +1,50 @@
+<?php
+
+$server = "localhost";
+$userid = "u9rnmkwnhqk3j";
+$pw = "#mWm2;z6h1m7";
+$db: "dbygr11xzpv4y5";
+
+$conn = new mysqli($server, $userid, $pw, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Check if form is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Hash the password
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+    // Check if user already exists
+    $stmt = $conn->prepare("SELECT * FROM user_table WHERE email = ?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        // User already exists
+        echo 'User with this email already exists';
+    } else {
+        // Insert user into the database
+        $stmt = $conn->prepare("INSERT INTO user_table (email, password) VALUES (?, ?)");
+        $stmt->bind_param("ss", $email, $hashedPassword);
+
+        if ($stmt->execute()) {
+            // Registration successful
+            header("Location: user_profile.html");
+            exit();
+        } else {
+            // Registration failed
+            $status = 'failure';
+            header("Location: registration.html?status=$status");
+            exit();
+        }
+    }
+}
+
+
+?>
--- user_auth.php
+++ user_auth.php
@@ -1,11 +1,24 @@
 <?php
 session_start();
 
-// Dummy data
-$users = [
-    'a@a.com' => 'pumpkin',
-    'user2' => 'password2'
-];
+// Database + test user
+$server = "localhost";
+$userid = "u9rnmkwnhqk3j";
+$pw = "#mWm2;z6h1m7";
+$db: "dbygr11xzpv4y5";
+
+$conn = new mysqli($server, $userid, $pw, $db);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+// // Dummy data
+// $users = [
+//     'a@a.com' => 'pumpkin',
+//     'user2' => 'password2'
+// ];
 
 // Check if form is submitted
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
