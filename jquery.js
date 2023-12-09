400a043d63a81c6f08c62330876d86d706c328d4
lydiachen01 <137934459+lydiachen01@users.noreply.github.com>
2023-12-09 12:02:23 -0500

    update

--- jquery.js
+++ jquery.js
@@ -8,10 +8,10 @@ document.addEventListener('DOMContentLoaded', function () {
         mobileMenu.classList.toggle('hidden');
     });
 
-    // Profile dropdown toggle
-    var profileButton = document.getElementById('user-menu-button');
-    var profileMenu = document.getElementById('profile-menu');
-    profileButton.addEventListener('click', function () {
-        profileMenu.classList.toggle('hidden');
-    });
+    // // Profile dropdown toggle
+    // var profileButton = document.getElementById('user-menu-button');
+    // var profileMenu = document.getElementById('profile-menu');
+    // profileButton.addEventListener('click', function () {
+    //     profileMenu.classList.toggle('hidden');
+    // });
 });
