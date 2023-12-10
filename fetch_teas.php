569f370a3957ed98c8b2de3f6ec11418717f6aab
VarleenBiketi <ayumabiketi96@gmail.com>
2023-12-10 00:08:56 -0500

    catalogue stuff

--- fetch_teas.php
+++ fetch_teas.php
@@ -2,7 +2,7 @@
 
 // Include your database connection code or configuration file here
 
-$conn = new mysqli("localhost","u9rnmkwnhqk3j","@*2l@2f7i%&2","dbygr11xzpv4y5" );
+$conn = new mysqli("localhost", "u9rnmkwnhqk3j", "@*2l@2f7i%&2", "dbygr11xzpv4y5");
 
 // Check connection
 if ($conn->connect_error) {
@@ -11,14 +11,18 @@ if ($conn->connect_error) {
     echo json_encode(['error' => 'Connection failed: ' . $conn->connect_error]);
     exit;
 }
+
 // Query to fetch tea names from the database
-$sql = "SELECT productName AS name, description , image FROM product_table";
+$sql = "SELECT productName AS name, image, description FROM product_table";
 $result = $conn->query($sql);
 
 // Fetch tea names into an array of objects
-$teas = array();
-while ($row = $result->fetch_assoc()) {
-    $teas[] = array('name' => $row['name']);
+while ($row = mysqli_fetch_assoc($result)) {
+    $teas[] = array(
+        'name' => $row['name'],
+        'description' => $row['description'],
+        'image' => $row['image']
+    );
 }
 
 // Set the response header to indicate JSON content
