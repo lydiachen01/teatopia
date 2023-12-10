b4d22d327fa39d3bd37b4d9d4b5e878d5e766c52
lwong10 <lucas.wong@tufts.edu>
2023-12-10 03:08:24 -0500

    made discussion change

--- discussion.js
+++ discussion.js
@@ -2,6 +2,24 @@ document.addEventListener('DOMContentLoaded', function () {
     const postForm = document.getElementById('postForm');
     const postsContainer = document.getElementById('posts');
 
+    // Function to create a post element
+    function createPostElement(post) {
+        const postElement = document.createElement('div');
+        postElement.innerHTML = `<strong>${post.username}</strong>: <span style="font-weight: bold;">${post.title}</span> - ${post.content}`;
+        return postElement;
+    }
+
+    // Fetch initial posts
+    fetch('fetch_posts.php')
+        .then(response => response.json())
+        .then(posts => {
+            posts.forEach(post => {
+                const postElement = createPostElement(post);
+                postsContainer.appendChild(postElement);
+            });
+        })
+        .catch(error => console.error('Error fetching posts:', error));
+
     postForm.addEventListener('submit', function (event) {
         event.preventDefault();
 
@@ -19,52 +37,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 // Update posts container with new post
                 const postElement = createPostElement(data);
-                postsContainer.insertBefore(postElement, postsContainer.firstChild);
+                postsContainer.appendChild(postElement);
 
                 // Clear the form
                 postForm.reset();
             })
             .catch(error => console.error('Error:', error));
     });
-
-    // Fetch initial posts (if any)
-    fetch('fetch_posts.php')
-        .then(response => response.json())
-        .then(posts => {
-            posts.reverse().forEach(post => {
-                const postElement = createPostElement(post);
-                postsContainer.appendChild(postElement);
-            });
-        })
-        .catch(error => console.error('Error:', error));
 });
-
-function createPostElement(post) {
-    const postContainer = document.createElement('div');
-
-    // Style for the username
-    const usernameElement = document.createElement('div');
-    usernameElement.style.fontSize = '18px';
-    usernameElement.style.fontWeight = 'bold';
-    usernameElement.textContent = post.username;
-
-    // Style for the title
-    const titleElement = document.createElement('div');
-    titleElement.style.fontSize = '14px';
-    titleElement.style.fontWeight = 'bold';
-    titleElement.textContent = post.title;
-
-    // Style for the content
-    const contentElement = document.createElement('div');
-    contentElement.textContent = post.content;
-
-    // Append elements to the post container
-    postContainer.appendChild(usernameElement);
-    postContainer.appendChild(titleElement);
-    postContainer.appendChild(contentElement);
-
-    // Apply some margin between posts
-    postContainer.style.marginBottom = '20px';
-
-    return postContainer;
-}
