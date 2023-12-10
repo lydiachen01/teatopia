80bbfe42949ed55f9531d0e3a583147645dd54dd
lwong10 <lucas.wong@tufts.edu>
2023-12-10 02:12:09 -0500

    checkout change

--- checkout.php
+++ checkout.php
@@ -11,7 +11,7 @@
      <!-- Bootstrap CDN -->
     
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 
     <link rel="stylesheet" href="styles.css">
