5ec40a0ed83fc29558051987eddd7497a808a890
VarleenBiketi <ayumabiketi96@gmail.com>
2023-12-10 16:28:27 -0500

    checkout page updates

--- checkout.php
+++ checkout.php
@@ -1,34 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    
-     <!-- Bootstrap CDN -->
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-     <!-- Bootstrap CDN -->
-    
+
+    <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Bootstrap CDN -->
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+        crossorigin="anonymous"></script>
 
     <link rel="stylesheet" href="styles.css">
-    <title>Shopping Cart 🍵</title>
+    <title>Checkout Page 🍵</title>
     <style>
         #checkoutPage {
-            margin: 0 150px; 
-            padding-top: 120px;
+            padding-top: 80px;
+            margin: 0 250px;
         }
+
         li {
             margin: 10px;
         }
+
         strong {
-            display:block;
+            display: block;
             text-align: right;
             margin-right: 20px;
         }
+
         button {
             background-color: #416843;
             color: #fff;
@@ -37,24 +43,53 @@
             padding: 10px;
             cursor: pointer;
         }
+
         button:hover {
-            background-color: #094f23; 
+            background-color: #094f23;
         }
+
         #thankYouMessage {
             font-size: 50px;
             margin: 60px auto;
             width: 50%;
             text-align: center;
         }
+
         .hide-text {
             display: none;
         }
-    
+
         .navbar-btn {
             background-color: transparent;
         }
+
+        #cTitle {
+            font-weight: bold;
+            font-size: 30px;
+        }
+        .minorTitle{
+            font-size: 20px;
+            font-weight:500;
+        }
+
+
+        /* Responsive Styles */
+        @media (max-width: 576px) {
+            #checkoutPage {
+                margin: 0 125px;
+            }
+
+            #thankYouMessage {
+                width: 80%;
+            }
+        }
     </style>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+        crossorigin="anonymous">
 </head>
+
 <body>
     <!-- Navbar Component -->
     <div id="header" style="z-index: 100;"></div>
@@ -66,20 +101,116 @@
 
     <div id="checkoutPage">
         <h1 style="text-align: center;" id="cTitle">Checkout</h1>
-        <ol id="checkout-items">
-            <!-- Cart items will be displayed here -->
-        </ol>
-        <p id="subtotal">Subtotal: $0.00</p>
-        <p id="tax">Tax (6.25%): $0.00</p>
-        <p id="total" style="font-weight: 1000;">Total: $0.00</p>
 
-        <button id="thankYouButton">Complete Purchase</button>
+        <!-- Shipping Address Form -->
+        <div id="shippingAddress" class="mb-2">
+            <h2 class= "minorTitle">Shipping Address</h2>
+            <form id="shippingForm">
+                <div class="mb-2">
+                    <label for="fullName" class="form-label">Full Name:</label>
+                    <input type="text" class="form-control" id="fullName" name="fullName" style="max-width: 100%;" required>
+                </div>
+
+                <div class="mb-2">
+                    <label for="address" class="form-label">Address:</label>
+                    <input type="text" class="form-control" id="address" name="address" style="max-width: 100%;" required>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6 mb-2">
+                        <label for="city" class="form-label">City:</label>
+                        <input type="text" class="form-control" id="city" name="city" style="max-width: 100%;" required>
+                    </div>
+
+                    <div class="col-md-4 mb-2">
+                        <label for="state" class="form-label">State:</label>
+                        <input type="text" class="form-control" id="state" name="state" style="max-width: 100%;" required>
+                    </div>
+
+                    <div class="col-md-2 mb-2">
+                        <label for="zipCode" class="form-label">Zip Code:</label>
+                        <input type="text" class="form-control" id="zipCode" name="zipCode" style="max-width: 100%;" required>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+        <!-- Payment Info Form -->
+        <div id="paymentInfo" class="mb-2">
+            <h2 class= "minorTitle">Payment Information</h2>
+            <form id="paymentForm">
+                <div class="mb-2">
+                    <label for="cardNumber" class="form-label">Card Number:</label>
+                    <input type="text" class="form-control" id="cardNumber" name="cardNumber" style="max-width: 100%;" required>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6 mb-2">
+                        <label for="expiryDate" class="form-label">Expiry Date:</label>
+                        <input type="text" class="form-control" id="expiryDate" name="expiryDate" style="max-width: 100%;"
+                            placeholder="MM/YYYY" required>
+                    </div>
+
+                    <div class="col-md-6 mb-2">
+                        <label for="cvv" class="form-label">CVV:</label>
+                        <input type="text" class="form-control" id="cvv" name="cvv" style="max-width: 100%;" required>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+        <hr><br/>
+
+        <!-- Cart Items -->
+        <div id="cartItems" class="mb-2">
+            <h2 class= "minorTitle">Cart Items</h2>
+            <ol id="checkout-items">
+                <!-- Cart items will be displayed here -->
+            </ol>
+        </div>
+
+        <!-- Subtotals and Totals -->
+        <div id="totals" class="mb-2">
+            <h2 class= "minorTitle">Totals</h2>
+            <p id="subtotal">Subtotal: $0.00</p>
+            <p id="tax">Tax (6.25%): $0.00</p>
+            <p id="total" style="font-weight: 1000;">Total: $0.00</p>
+        </div>
+
+        <div class="d-flex justify-content-center mb-2"> <!-- Added container div -->
+            <button id="thankYouButton" class="btn btn-success">Complete Purchase</button>
+        </div>
+
+
+        <!-- Thank You Message -->
+        <p id="thankYouMessage" class="hide-text">Thank you for shopping with us!!</p>
     </div>
 
-    <p id="thankYouMessage" style="display: none;">Thank you for shopping with us!!</p>
+    
 
     <script>
         let cart = {};
+
+        function hideElements() {
+            const checkoutItems = document.getElementById('checkout-items');
+            const subtotalElement = document.getElementById('subtotal');
+            const taxElement = document.getElementById('tax');
+            const totalElement = document.getElementById('total');
+            const thankYouButton = document.getElementById('thankYouButton');
+            const checkoutTitle = document.getElementById('cTitle');
+
+            checkoutItems.classList.add('hide-text');
+            subtotalElement.classList.add('hide-text');
+            taxElement.classList.add('hide-text');
+            totalElement.classList.add('hide-text');
+            thankYouButton.classList.add('hide-text');
+            checkoutTitle.classList.add('hide-text');
+        }
+
+        function showThankYouMessage() {
+            const thankYouMessage = document.getElementById('thankYouMessage');
+            thankYouMessage.style.display = 'block';
+        }
 
         document.addEventListener("DOMContentLoaded", function () {
             const checkoutItems = document.getElementById('checkout-items');
@@ -87,28 +218,28 @@
             const taxElement = document.getElementById('tax');
             const totalElement = document.getElementById('total');
             const thankYouButton = document.getElementById('thankYouButton');
-            const thankYouMessage = document.getElementById('thankYouMessage');
             const checkoutTitle = document.getElementById('cTitle');
 
             <?php
 
-            $conn = new mysqli("localhost", "u9rnmkwnhqk3j", "@*2l@2f7i%&2", "dbygr11xzpv4y5");
+                $conn = new mysqli("localhost", "u9rnmkwnhqk3j", "@*2l@2f7i%&2", "dbygr11xzpv4y5");
 
-            if (!$conn) {
-                die("Connection failed: " . mysqli_connect_error());
-            }
+                if (!$conn) {
+                    die("Connection failed: " . mysqli_connect_error());
+                }
 
-            $query = "SELECT productName, price FROM product_table";
-            $result = mysqli_query($conn, $query);
+                $query = "SELECT productName, price FROM product_table";
+                $result = mysqli_query($conn, $query);
 
-            $prices = array();
+                $prices = array();
 
-            while ($row = mysqli_fetch_assoc($result)) {
-                $prices[$row['productName']] = $row['price'];
-            }
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $prices[$row['productName']] = $row['price'];
+                }
 
-            mysqli_close($conn);
-            ?>
+                mysqli_close($conn);
+                ?>
+
 
             const teaPrices = <?php echo json_encode($prices); ?>;
 
@@ -118,7 +249,7 @@
             }
 
             const cartData = getUrlParameter('cartData');
-            
+
             if (cartData) {
                 cart = JSON.parse(decodeURIComponent(cartData));
                 console.log('Cart:', cart);
@@ -151,13 +282,8 @@
             }
 
             thankYouButton.addEventListener('click', function () {
-                thankYouMessage.style.display = 'block';
-                checkoutItems.classList.add('hide-text');
-                subtotalElement.classList.add('hide-text');
-                taxElement.classList.add('hide-text');
-                totalElement.classList.add('hide-text');
-                thankYouButton.classList.add('hide-text');
-                checkoutTitle.classList.add('hide-text');
+                hideElements();
+                showThankYouMessage();
 
                 setTimeout(function () {
                     window.location.href = 'index.html';
@@ -166,7 +292,11 @@
         });
     </script>
 
-    <!-- Footer -->
 
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Footer -->
 </body>
+
 </html>
